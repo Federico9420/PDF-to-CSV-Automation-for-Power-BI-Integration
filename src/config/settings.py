19186251270d7eb2e42b42135007c10892369a1a
Federico Javier Almonacid <federicoalmonacid.laboral@gmail.com>
2025-09-28 16:15:42 -0300

    static configs changes3

--- src/config/settings.py
+++ src/config/settings.py
@@ -7,7 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'DEV-ONLY-CHANGE-ME')
 # SECURITY WARNING: don't run with debug turned on in production!
-STATICFILES_DIRS = [BASE_DIR / 'static']
 DEBUG = False  
 ALLOWED_HOSTS = ['Fede9420.pythonanywhere.com']
 #ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
