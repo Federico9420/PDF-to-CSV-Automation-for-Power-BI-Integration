a327d966f7897a60f599be42d27f010a51c2fc42
Federico Javier Almonacid <federicoalmonacid.laboral@gmail.com>
2025-10-01 04:54:45 -0300

    changes

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # 🔑 Seguridad
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'DEV-ONLY-CHANGE-ME')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['fede9420.pythonanywhere.com']
 
 # 📦 Apps
 INSTALLED_APPS = [
